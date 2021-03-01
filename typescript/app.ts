6971ff5656e632fd54f0b11191f216a704310e96
Reinaldo <reinaldoweb@hotmail.com>
2021-03-01 13:59:56 -0300

    Trablhando com let

--- typescript/app.ts
+++ typescript/app.ts
@@ -4,7 +4,7 @@ let estudandoAngular: boolean = true
 
 
 let listaDeFrutas: Array<string> = ['Uva','BAnana','Abacaxi']
-let listaDeFrutas2: string[] = ['Uva','BAnana','Abacaxi']
+let listaDeFrutas2: string[] = ['Uva','Banana','Abacaxi']
 
 let notasDasProvas: Array<number> = [7,5,8,9]
 let notasDasProvas2: number[] = [7,5,8,9]
