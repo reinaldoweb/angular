d36d30649638b5a541231da01f6348c096baa7aa
Reinaldo <reinaldoweb@hotmail.com>
2021-03-08 17:18:24 -0300

    Importando frase para o componente Painel

--- projetos/app1/src/app/painel/painel.component.ts
+++ projetos/app1/src/app/painel/painel.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Frase } from '../shared/frase.model'
+import { FRASES } from './frases-mock'
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css']
 })
+
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  public frases: Frase[] = FRASES
+
+
+  constructor() { console.log(this.frases) }
 
   ngOnInit(): void {
   }
